41a031412162f9492ff24955888711ae210bda48
Anand P <anandpamegoze@gmail.com>
2024-10-09 02:07:01 +0530

    change vite config, add port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.PORT || 3000,
+    host: true,
     proxy: {
       '/api': {
         target: 'https://www.perplexity.ai',
